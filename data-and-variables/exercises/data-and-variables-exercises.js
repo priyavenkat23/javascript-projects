33ca83608980300740e5d6f6a2e9888be3c47cf4
priyavenkat23 <shanmugapriya.gunaseelan@gmail.com>
2024-01-05 13:09:58 -0600

    Calculate Space Mission

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,11 +1,23 @@
 // Declare and assign the variables below
+let kilometersToMars=22505000;
+let milesPerKilometer = 22505000 / 1.6;
+const shuttleSpeedMph= 24600;
+const shuttleName = "Chandrayan";
 
 // Use console.log to print the 'typeof' each variable. Print one item per line.
+console.log(typeof kilometersToMars);
+console.log(typeof milesPerKilometer);
 
 // Calculate a space mission below
+let milesToMars = kilometersToMars * milesPerKilometer;
+let hoursToMars = milesToMars / shuttleSpeedMph;
 
 // Print the results of the space mission calculations below
+console.log("Miles to Mars: "+milesToMars);
+console.log("Hours to Mars: " +hoursToMars);
 
 // Calculate a trip to the moon below
+let daysToMars = hoursToMars / 24;
 
 // Print the results of the trip to the moon below
+console.log(shuttleName +" will take "+ daysToMars + " days to reach Mars.");
